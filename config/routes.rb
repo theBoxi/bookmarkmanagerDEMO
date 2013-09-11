2dbb4cc97265e25367fd07415437a32a92416dc2
theBoxi <online@boxi.ch>
2013-09-11 22:23:43 +0200

    replaced differnt routs with one resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Bookmarkmanager::Application.routes.draw do
-  get "bookmarks" => "bookmarks#index", as: "bookmarks"
-  get "bookmarks/new", as: "new_bookmark"
-  get "bookmarks/:id" => "bookmarks#show", as: "bookmark"
-  get "bookmarks/:id/edit" => "bookmarks#edit", as: "edit_bookmark"
-  post "bookmarks" => "bookmarks#create"
-  put "bookmarks/:id" => "bookmarks#update"
-  delete "bookmarks/:id" => "bookmarks#destroy", as: "bookmark"
+  resources :bookmarks
 end

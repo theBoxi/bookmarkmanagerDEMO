08fb53b0de3dc99426dc1b2a70e6e38c8783eacc
theBoxi <online@boxi.ch>
2013-09-11 22:17:20 +0200

    fix some Typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Bookmarkmanager::Application.routes.draw do
-  get "bookmarks/:id/edit" => "bookmarks#edit", as: "edit_bookmark"
-
-  get "bookmarks/new", as: "new_bookmark"
-
   get "bookmarks" => "bookmarks#index", as: "bookmarks"
-
+  get "bookmarks/new", as: "new_bookmark"
   get "bookmarks/:id" => "bookmarks#show", as: "bookmark"
-
+  get "bookmarks/:id/edit" => "bookmarks#edit", as: "edit_bookmark"
   post "bookmarks" => "bookmarks#create"
-
   put "bookmarks/:id" => "bookmarks#update"
-
-  delete "bookmark/:id" => "bookmarks#destroy", as: "bookmark"
+  delete "bookmarks/:id" => "bookmarks#destroy", as: "bookmark"
 end

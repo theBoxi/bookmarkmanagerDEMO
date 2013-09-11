6aaccb2aff80bb384947a07b47640d1ac30b43f3
theBoxi <online@boxi.ch>
2013-09-11 22:05:04 +0200

    finished CRUD

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,12 +4,38 @@ class BookmarksController < ApplicationController
   end
 
   def show
-    @bookmark = Bookmark.find(params[:id]);
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update_attributes(params[:bookmark])
+      redirect_to bookmarks_path
+    else
+      render "edit"
+    end
   end
 
   def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params[:bookmark])
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Bookmarkmanager::Application.routes.draw do
-  get "bookmarks/edit"
+  get "bookmarks/:id/edit" => "bookmarks#edit", as: "edit_bookmark"
 
-  get "bookmarks/new"
+  get "bookmarks/new", as: "new_bookmark"
 
-  get "bookmarks" => "bookmarks#index"
+  get "bookmarks" => "bookmarks#index", as: "bookmarks"
 
-  get "bookmarks/:id" => "bookmarks#show"
+  get "bookmarks/:id" => "bookmarks#show", as: "bookmark"
+
+  post "bookmarks" => "bookmarks#create"
+
+  put "bookmarks/:id" => "bookmarks#update"
+
+  delete "bookmark/:id" => "bookmarks#destroy", as: "bookmark"
 end
